39e763113f88302e0636c716a86a1d8fc9030974
bbytes-dev <bharathbyahatti8050@gmail.com>
2026-05-11 00:35:10 +0530

    revamp of the mono repo

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,1 +1,81 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { authMiddleware } from "./middleware";
+import { prismaClient } from "db/client";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/api/v1/website", authMiddleware, async (req, res) => {
+  const userId = req.userId!;
+  const { url } = req.body;
+
+  const data = await prismaClient.website.create({
+    data: {
+      userId,
+      url
+    },
+  });
+
+  res.json({
+    id: data.id,
+    message: "Website added successfully"
+  })
+
+});
+
+app.get("/api/v1/websites/status", authMiddleware, async (req, res) => {
+  const websiteId = req.query.websiteId;
+  const userId = req.userId!;
+  const data = await prismaClient.website.findFirst({
+    where: {
+      id: websiteId as string,
+      userId,
+      disabled:false
+    },include :{
+      ticks: true
+    }
+  })
+
+  res.json(data)
+
+});
+
+app.get("/api/v1/websites", authMiddleware, async(req, res) => {
+  const userId = req.userId!;
+  const data = await prismaClient.website.findMany({
+    where: {
+      userId,
+      disabled:false
+    }
+  })
+  res.json(data)
+});
+
+
+app.delete("/api/v1/website", authMiddleware, async (req, res) => {
+  const userId = req.userId!;
+const websiteId = req.query.websiteId as string;
+  const data = await prismaClient.website.update({
+    where: {
+      userId,
+      id: websiteId
+    },
+    data: {
+      disabled: true
+    }
+  })
+  res.json({message: "Website deleted successfully"})
+});
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
---
+++ packages/db/prisma/migrations/20260510090834_init/migration.sql
@@ -0,0 +1,53 @@
+-- CreateEnum
+CREATE TYPE "WebsiteStatus" AS ENUM ('Good', 'Bad');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Website" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Website_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Validator" (
+    "id" TEXT NOT NULL,
+    "publicKey" TEXT NOT NULL,
+    "ipAddress" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Validator_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WebSiteUptimeStatus" (
+    "id" TEXT NOT NULL,
+    "websiteId" TEXT NOT NULL,
+    "validatorId" TEXT NOT NULL,
+    "status" "WebsiteStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "WebSiteUptimeStatus_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Validator_publicKey_key" ON "Validator"("publicKey");
+
+-- AddForeignKey
+ALTER TABLE "WebSiteUptimeStatus" ADD CONSTRAINT "WebSiteUptimeStatus_websiteId_fkey" FOREIGN KEY ("websiteId") REFERENCES "Website"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WebSiteUptimeStatus" ADD CONSTRAINT "WebSiteUptimeStatus_validatorId_fkey" FOREIGN KEY ("validatorId") REFERENCES "Validator"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
